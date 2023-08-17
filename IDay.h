365989c21ac44bd232ab06e9d5f8f1089ff68a16
Felix Lidö <felix.lido@gmail.com>
2023-08-17 21:46:18 +0200

    created an Interface for use by puzzle solvers

--- IDay.h
+++ IDay.h
@@ -1,16 +1,18 @@
 //
 // Created by felix on 2023-08-17.
 //
-
 #ifndef ADVENTOFCODECPP_IDAY_H
 #define ADVENTOFCODECPP_IDAY_H
 
-
+#include "iostream"
 
 class IDay {
-
+public:
+    void print(){
+        std::cout << "Part 1: " << part1() << ", Part 2: " << part2() << std::endl;
+    };
+private:
+    virtual int part1() = 0;
+    virtual int part2() = 0;
 };
-
-
-
 #endif //ADVENTOFCODECPP_IDAY_H
